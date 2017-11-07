4c9800c24c3d79fa9ba0646f4d3371d0c774f425
Joanna <dinh2@umbc.com>
2017-11-06 23:17:47 -0500

    deleting..

--- districtlands/example/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework.decorators import api_view
-from django.views.decorators.csrf import csrf_exempt
-
-# Create your views here.
-
-def something(request):
-    return HttpResponse("something")
