1859c40361843975e9a132d426608a688b252f83
Joanna <dinh2@umbc.com>
2017-11-06 23:20:33 -0500

    adding backend again...

---
+++ example/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
+
+# Create your views here.
+
+def something(request):
+    return HttpResponse("something")
