bfc06fa3eee5df6bd7cfa913c30ec4ed9858c195
Joanna <dinh2@umbc.com>
2017-11-07 01:40:28 -0500

    .......

--- districtlands/settings.py
+++ districtlands/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'districtlands.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config()
+DATABASES = { 
+	'default' = dj_database_url.config()
+}
 
 #DATABASES = {
 	
