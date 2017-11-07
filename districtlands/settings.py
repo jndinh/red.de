5f29c30777abc4e85b76b974a8108bbbcac66cda
Joanna <dinh2@umbc.com>
2017-11-07 01:37:26 -0500

    try 1243123

--- districtlands/settings.py
+++ districtlands/settings.py
@@ -78,13 +78,15 @@ WSGI_APPLICATION = 'districtlands.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-    	dj_database_url.config()
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES[‘default’] = dj_database_url.config()
+
+#DATABASES = {
+	
+ #   'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #   }
+#}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
