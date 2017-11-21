48a7f95a03e6c8c53b7835a378f761aad2dc91e8
linstephen13 <lin14@umbc.edu>
2017-11-21 17:05:24 -0500

    Formatting, removed trailing spaces

--- algorithim/districts.py
+++ algorithim/districts.py
@@ -24,7 +24,7 @@ number of districts - Maryland is only assigned 8 congressional districts,
                       or fewer. should be easy to fix (maybe)
 
 CHANGE LOG
-Dorothy Carter - 20171109 - initial creation           
+Dorothy Carter - 20171109 - initial creation
 
 '''
 
@@ -58,7 +58,7 @@ def _create_district(start):
     arguments: start - a tract object from which districting starts
     returns: a district object representing the finished district
     '''
-    
+
     # tries to add the passed-in starting tract to the district
     # will raise an exception if the district is already taken
     created_district = geography_objects.district()
@@ -66,24 +66,24 @@ def _create_district(start):
         created_district.add_tract(start)
     else:
         raise geography_objects.district_error("starting tract is already taken")
-    
+
     # current stores the last tract added to district
     current = start
-    
+
     # this loop keeps trying to add tracts until the district hits
-    # its population target. see ISSUES above for the potential 
+    # its population target. see ISSUES above for the potential
     # infinite loop issue
     while created_district.population <= MAGIC_POPULATION_NUMBER:
         next = all_tracts[random.choice(current.adjacent_to)]
-        
+
         if _take_tract(next.id):
             created_district.add_tract(next)
             current = next
         else:
             print("fail")
-    
+
     return created_district
-    
+
 
 def generic_redistrict():
     '''
@@ -93,10 +93,11 @@ def generic_redistrict():
     start = all_tracts["751200"]
     return specific_redistrict(start)
 
+
 def specific_redistrict(start):
     '''
     this redistricts Maryland using a given start tract
     arguments: start - a tract object that districting will start from
-    returns: 
+    returns:
     '''
     return "not implemented"
--- algorithim/geography_objects.py
+++ algorithim/geography_objects.py
@@ -32,57 +32,57 @@ class tract:
         self.population = pop
         self.id = tract_id
         self.adjacent_to = []
-    
+
     def add_adjacency(self, tract):
         '''
         this appends a tract to the list of adjacent tracts
-        
+
         arguments: tract. A str (the tract id). No default
         '''
-        
+
         self.adjacent_to.append(tract)
-    
+
     def set_adjacencies_to(self, areas):
         '''
         this is a convenience to set the adjacent_to attribute to a new list
         provided in case extra processing has to go here
-        
+
         arguments: areas. A list of tract ids (strs) it is adjacent to. No default
         '''
-        
+
         self.adjacent_to = areas
-        
+
     def bulk_add_adjacencies(self, tracts):
         '''
         this appends a list of tracts to the list of adjacent tracts
-        
+
         arguments: tracts. A list of tract ids (strs) it is adjacent to. No default
         '''
-        
+
         self.adjacent_to.extend(tracts)
-   
+
     def get_adjacencies(self):
         '''
         this returns the adjacent_to list. not strictly necessary (could use self.adjacent_to),
         but provided as a convenience
-        
+
         can use as:
         for tract in this_tract.get_adjacencies():
             pass
         '''
         return self.adjacent_to
-   
+
     def __str__(self):
         '''
         this prints the tract object nicely.
         '''
         return "Census tract " + self.id + ". Population: " + str(self.population)
-    
+
     def __eq__(self, other):
         '''
         this compares tract ids to determine equality
         '''
-        
+
         if hasattr(other, "id"):
             return self.id == other.id
         else:
@@ -98,7 +98,7 @@ class district:
         '''
         self.population = pop
         self.tracts = []
-    
+
     def add_tract(self, tract_id, tract_pop):
         '''
         this initializes a new tract object and adds it to the tracts list
@@ -107,7 +107,7 @@ class district:
         '''
         self.tracts.append( tract(tract_pop, tract_id) )
         self.population += tract_pop
-    
+
     def add_tract(self, tract):
         '''
         this adds a previously created tract object to the tracts list
@@ -115,18 +115,17 @@ class district:
         '''
         self.tracts.append(tract)
         self.population += tract.population
-   
+
     def bulk_add_tract(self, many_tracts):
         '''
         this adds a list of tracts to the component tracts
         arguments: many_tracts. A list of tracts. No default
         '''
         self.tracts.extend(many_tracts)
-        
+
         for tract in many_tracts:
             self.population += tract.population
-        
-        
+
     def set_tracts_to(self, new_tracts):
         '''
         this sets the list of component tracts to the list passed in
@@ -134,17 +133,17 @@ class district:
         arguments: new_tracts. A list of tracts. No default
         '''
         self.tracts = new_tracts
-        
+
         for tract in new_tracts:
             self.population += tract.population
-   
+
     def get_tracts(self):
         '''
         this returns the list of component tracts. not strictly necessary (use self.tracts),
         but provided as a convenience
         '''
         return self.tracts
-        
+
     def __str__(self):
         '''
         this prints the district object nicely
@@ -157,7 +156,7 @@ class district_error(Exception):
         '''
         this defines an error originating in these modules,
         to distinguish from runtime, etc
-        
+
         arguments: msg - str - an explanation of the exception
         '''
         super.__init__(msg)
