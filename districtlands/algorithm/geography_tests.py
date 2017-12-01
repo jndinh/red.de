0ad92acfb510076ea10f5b6582ee23b2698d5665
linstephen13 <lin14@umbc.edu>
2017-12-01 15:14:31 -0500

    Introduced district stealing methods

--- districtlands/algorithm/geography_tests.py
+++ districtlands/algorithm/geography_tests.py
@@ -9,7 +9,7 @@ from districts import all_tracts
 def test_single_district():
     final = districts._create_district(all_tracts["751200"])
     print(final)
-    
+
 def test_total_redistricting():
     redistrict = districts.generic_redistrict()
     print(redistrict)
@@ -17,11 +17,11 @@ def test_total_redistricting():
 def test_density_score():
     score = districts._density(all_tracts["751200"])
     print(score)
-    
+
 def test_test_redistrict():
     redistrict = districts._test_redistrict()
     print(redistrict)
-    
+
 
 if __name__ == "__main__":
     test_test_redistrict()
