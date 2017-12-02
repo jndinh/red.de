bc20ffc3f439c9e4eead32655ca5aa7c4e9ed0f1
Joanna <dinh2@umbc.com>
2017-12-01 19:57:06 -0500

    debug

--- districtlands/views.py
+++ districtlands/views.py
@@ -28,13 +28,13 @@ def user_start(request):
     Using a predetermined starting point, send back a list of districts that have been redrawn.
     '''
     #try:
-    tract_id = request.POST.get('tract_id', '')
+    tract_id = request.POST.get('tract_id')
 
     # Check for tract_id to start algorithm
     if tract_id is None:
         return JsonResponse({"detail": "tract_id missing."}, content_type = "application/json", status = 400)
 
-    new_districts = districts.specific_redistrict(tract_id)
+    new_districts = districts.specific_redistrict(str(tract_id))
 
     return JsonResponse({'detail' : 'Districts created.', 'districts' : new_districts}, content_type="application/json", status=200)
     #except Exception as e:
